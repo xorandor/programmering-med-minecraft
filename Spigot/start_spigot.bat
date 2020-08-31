131017124368f74a1d6bd8f2af8a5e7cb9c7c837
Andreas Schmidt Jensen <andreas@nivo.nu>
2020-08-31 21:46:44 +0200

    Spigot og eksempel tilføjet

---
+++ Spigot/start_spigot.bat
@@ -0,0 +1,2 @@
+java -Xms512M -Xmx1G -jar spigot-1.15.2.jar -o true 
+pause
---
+++ code/example.py
@@ -0,0 +1,18 @@
+import mcpi.minecraft as minecraft
+import mcpi.block as block
+mc = minecraft.Minecraft.create()
+mc.postToChat("Hello World")
+pos = mc.player.getPos()
+print(pos.x)
+"""
+for i in range(0, 100):
+    mc.setBlock(pos.x+i, pos.y, pos.z, block.TNT.id)
+    mc.setBlock(pos.x+i, pos.y+1, pos.z, block.FIRE.id)
+"""
+# create stone shell
+mc.setBlocks(pos.x, pos.y, pos.z, pos.x+5, pos.y+5, pos.z+5, block.STONE.id)
+# fill with tnt
+mc.setBlocks(pos.x+1, pos.y+1, pos.z+1, pos.x+5-1,
+             pos.y+5-1, pos.z+5-1, block.TNT.id)
+# switch out a top stone with fire
+mc.setBlock(pos.x+1, pos.y+5, pos.z+1, block.FIRE.id)
