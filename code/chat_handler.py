2c513fd91a21e345da9267fa0714c36452514e0f
jonasholbech <jh@jonasholbech.dk>
2021-08-13 09:44:07 +0200

    committed from github.dev

--- code/chat_handler.py
+++ code/chat_handler.py
@@ -1,6 +1,6 @@
 from mcpi.minecraft import Minecraft
 
-
+# poll the server for the latest messages
 def get_latest_message(mc):
     chatEvents = mc.events.pollChatPosts()
     if len(chatEvents) > 0:
